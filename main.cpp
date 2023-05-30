31396a0817ae6d740bfe4d5be9e718cc27df28f6
Sidtel <133479655+Sidtel@users.noreply.github.com>
2023-05-30 12:36:03 +0530

    1st

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include "crow_all.h"
+#include <vector>
+#include <string>
+
+struct Employee {
+    std::string employeeId;
+    std::string name;
+    std::string city;
+};
+
+std::vector<Employee> employees;
+
+int main() {
+  crow::SimpleApp app;
+
+  CROW_ROUTE(app, "/greeting")([]() {
+    return "Hello world!";
+  });
+
+  CROW_ROUTE(app, "/employee").name("create_employee").methods("POST"_method)([](){
+   crow::json::wvalue response;
+
+    // Get the JSON payload from the request body
+    auto payload = crow::json::load(crow::request_body);
+
+    // Validate the payload
+    if (!payload) {
+        response["error"] = "Invalid JSON payload";
+        return crow::response(400, response);
+    }
+
+    // Extract the employee data from the payload
+    Employee employee;
+    employee.employeeId = payload["employeeId"].s();
+    employee.name = payload["name"].s();
+    employee.city = payload["city"].s();
+
+    // Add the employee to the vector
+    employees.push_back(employee);
+
+    // Prepare the response
+    response["message"] = "Employee created successfully";
+    response["employee"] = employee;
+
+    return crow::response(200, response);
+  });
+
+  CROW_ROUTE(app, "/employee/all").name("get_all_employee").methods("GET"_method)([](){
+    return "";
+  });
+
+  CROW_ROUTE(app, "/employee/<string>").name("get_employee").methods("GET"_method)([](const std::string& id){
+    return "";
+  });
+
+  CROW_ROUTE(app, "/employee/<string>").name("update_employee").methods("PUT"_method)([](const std::string& id){
+    return "";
+  });
+
+  CROW_ROUTE(app, "/employee/<string>").name("delete_employee").methods("DELETE"_method)([](const std::string& id){
+    return "";
+  });
+
+  app.port(8080).run();
+
+  return 0;
+}
